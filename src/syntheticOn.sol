5debdd7ba447f1352ed9b8099dbc3c749bb62f51
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-06-27 23:47:19 +0530

    OracleLib.sol File

--- src/syntheticOn.sol
+++ src/syntheticOn.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { FunctionsClient } from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/FunctionsClient.sol";
+import { FunctionsClient } from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
 import { ConfirmedOwner } from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
-import { FunctionsRequest } from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/libraries/FunctionsRequest.sol";
+import { FunctionsRequest } from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { OracleLib, AggregatorV3Interface } from "./libraries/OracleLib.sol";
 import { Pausable } from "@openzeppelin/contracts/utils/Pausable.sol";
